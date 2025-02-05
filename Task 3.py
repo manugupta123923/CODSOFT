09eae7e7842ac4f7573ca17eacfb7c860cf2de08
manugupta123923 <gstrani9918@gmail.com>
2025-02-05 23:37:33 +0530

    Delete Task 3.py

--- Task 3.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-length = int(input("Enter password length: "))
-password = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase+ string.digits + string.punctuation, k=length))
-print("Generated Password:",password)
