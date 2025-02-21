787ae7a45a69a619c4925cb681b754b549afab37
manugupta123923 <gstrani9918@gmail.com>
2025-02-21 20:24:17 +0530

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,9 @@
+# Task-3
+# PASSWORD GENERATOR
+
+import random
+import string
+
+length = int(input("Enter password length: "))
+password = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase+ string.digits + string.punctuation, k=length))
+print("Generated Password:",password)
